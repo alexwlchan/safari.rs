e8e1a51fb23ca44e80b5739b4f4ee3776f472aa8
Alex Chan <alex@alexwlchan.net>
2017-04-30 14:05:14 +0100

    Space out main.rs for better readability

--- src/main.rs
+++ src/main.rs
@@ -17,24 +17,31 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
   let args = cli::parse_args(NAME);
+
   if args.flag_version {
     println!("{} v{}", NAME, VERSION);
-  } else if args.cmd_url {
+  }
+
+  if args.cmd_url {
     safari::assert_safari_is_running();
     print!("{}", safari::get_url(args.flag_window, args.flag_tab));
-  } else if args.cmd_urls_all {
+  }
+
+  if args.cmd_urls_all {
     safari::assert_safari_is_running();
-    let urls = safari::get_all_urls();
-    for url in urls {
+    for url in safari::get_all_urls() {
       println!("{}", url);
     }
-  } else if args.cmd_close_tabs {
+  }
+
+  if args.cmd_close_tabs {
     safari::assert_safari_is_running();
     let patterns = args.arg_urls_to_close.split(",").collect();
     safari::close_tabs(patterns);
-  } else if args.cmd_reading_list {
-    let urls = safari::get_reading_list_urls();
-    for url in urls {
+  }
+
+  if args.cmd_reading_list {
+    for url in safari::get_reading_list_urls() {
       println!("{}", url);
     }
   }
