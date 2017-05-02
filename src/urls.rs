f8fc77d3ebed971f942ea75acbb461d6a697ee45
Alex Chan <alex@alexwlchan.net>
2017-05-02 11:53:30 +0100

    Tidy up whitespace in the existing tests

--- src/urls.rs
+++ src/urls.rs
@@ -125,24 +125,40 @@ macro_rules! tidy_url_tests {
 
 
 tidy_url_tests! {
-    url_tw_0:  ("https://mobile.twitter.com/Breaking911/status/822589852596191235",
-                "https://twitter.com/Breaking911/status/822589852596191235"),
-    url_tw_1:  ("https://twitter.com/elise3aum/status/822903824268533762",
-                "https://twitter.com/elise3aum/status/822903824268533762"),
+  twitter_mobile: (
+    "https://mobile.twitter.com/Breaking911/status/822589852596191235",
+    "https://twitter.com/Breaking911/status/822589852596191235"
+  ),
 
-    url_az_0:  ("https://www.amazon.co.uk/dp/B01DFKBL68/ref=gw_aucc_comb_AB_clean_2?pf_rd_r=N1MF2PWADRHS55427ETG&pf_rd_p=c11a2c11-c670-46ff-87a1-c1eef4fad652",
-                "https://www.amazon.co.uk/dp/B01DFKBL68/"),
+  regular_twitter: (
+    "https://twitter.com/WholesomeMeme/status/846421658835537921",
+    "https://twitter.com/WholesomeMeme/status/846421658835537921"
+  ),
 
-    url_md_0:  ("https://medium.com/@anildash/forget-why-its-time-to-get-to-work-c49ac5f0da20#.sjyskxdsz",
-                "https://medium.com/@anildash/forget-why-its-time-to-get-to-work-c49ac5f0da20"),
+  amazon_product: (
+    "https://www.amazon.co.uk/dp/B01DFKBL68/ref=gw_aucc_comb_AB_clean_2?pf_rd_r=N1MF2PWADRHS55427ETG&pf_rd_p=c11a2c11-c670-46ff-87a1-c1eef4fad652",
+    "https://www.amazon.co.uk/dp/B01DFKBL68/"
+  ),
 
-    url_tm_0:  ("http://azurelunatic.tumblr.com/post/155525051123/things-about-hufflepuffs-539#notes",
-                "http://azurelunatic.tumblr.com/post/155525051123/things-about-hufflepuffs-539"),
+  medium_with_tracking: (
+    "https://medium.com/@anildash/forget-why-its-time-to-get-to-work-c49ac5f0da20#.sjyskxdsz",
+    "https://medium.com/@anildash/forget-why-its-time-to-get-to-work-c49ac5f0da20"
+  ),
 
-    url_8:   ("https://www.buzzfeed.com/katienotopoulos/the-end-of-apple-man#.biqmkzz8kn",
-              "https://www.buzzfeed.com/katienotopoulos/the-end-of-apple-man"),
-    url_9:   ("http://mashable.com/2016/03/21/apple-liam-recycling-robot/#b9y4lv3m4qqX",
-              "http://mashable.com/2016/03/21/apple-liam-recycling-robot/"),
+  tumblr_with_notes: (
+    "http://azurelunatic.tumblr.com/post/155525051123/things-about-hufflepuffs-539#notes",
+    "http://azurelunatic.tumblr.com/post/155525051123/things-about-hufflepuffs-539"
+  ),
+
+  buzzfeed_with_tracking: (
+    "https://www.buzzfeed.com/katienotopoulos/the-end-of-apple-man#.biqmkzz8kn",
+    "https://www.buzzfeed.com/katienotopoulos/the-end-of-apple-man"
+  ),
+
+  mashable_with_tracking: (
+    "http://mashable.com/2016/03/21/apple-liam-recycling-robot/#b9y4lv3m4qqX",
+    "http://mashable.com/2016/03/21/apple-liam-recycling-robot/"
+  ),
 
   youtube_plain_video: (
     "https://www.youtube.com/watch?v=zB4I68XVPzQ",
